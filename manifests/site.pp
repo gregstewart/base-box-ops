05d37bafc25fef51d01722c87fd6a95932ad1109
Greg Stewart <gregs@tcias.co.uk>
2013-02-14 21:05:36 +0000

    Building out the NGinx resource declaration

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,19 @@
 Exec {
   path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 }
+package {
+  "nginx":
+     ensure => present
+}
+service {
+  "nginx":
+    ensure => true,
+    enable => true
+}
+file {
+  "/etc/nginx/nginx.conf":
+    source => "puppet:///modules/nginx/nginx.conf",
+    mode   => 644,
+    owner  => root,
+    group  => root
+}
