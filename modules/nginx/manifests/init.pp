4174e352c58f5253ed36c475cf61ca318c8d8a45
Greg Stewart <gregs@tcias.co.uk>
2013-02-14 21:16:21 +0000

    Refactoring of the nginx puppet file and added service restart on conf change

---
+++ modules/nginx/manifests/init.pp
@@ -0,0 +1,20 @@
+class nginx {
+	package {
+  		"nginx":
+     		ensure => present,
+		before => File["/etc/nginx/nginx.conf"]
+	}
+	service {
+  		"nginx":
+    		ensure => true,
+    		enable => true,
+		subscribe => File["/etc/nginx/nginx.conf"]
+	}
+	file {
+  		"/etc/nginx/nginx.conf":
+    		source => "puppet:///modules/nginx/nginx.conf",
+    		mode   => 644,
+    		owner  => root,
+    		group  => root
+	}
+}
